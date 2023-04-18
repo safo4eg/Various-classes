67c1d6e54f5696424c4a69b2f6d8670233172c72
safo4eg <stas.rakov111@mail.ru>
2023-04-18 18:49:48 +0300

    Создан класс Date, работающий на классе DateTime + DateInterval::createFromDateString и методе getdate().
    Дата в конструкторе должна иметь формат: "Y-m-d";
    
    Реализованы функции:
    получения дня, месяца года;
    прибавить/убавить n-e количество дней, месяцев, лет;
    смена формата даты(работает на DateTime->format();

---
+++ classes/Date.php
@@ -0,0 +1,80 @@
+<?php
+
+class Date {
+    private $date_time;
+    private $date_info;
+
+    public function __construct($input_date = null) {
+        if(isset($input_date)) $this->date_time = new DateTime($input_date);
+        else $this->date_time = new DateTime();
+        $this->date_info = getdate($this->date_time->getTimestamp());
+    }
+
+    public function getDay() {
+        return $this->date_info['mday'];
+    }
+
+    public function getMonth() {
+        return $this->date_info['mon'];
+    }
+
+    public function getYear() {
+        return $this->date_info['year'];
+    }
+
+    public function getWeekDay() {
+        $days_names = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+        return $days_names[$this->date_info['wday']];
+    }
+
+    public function addDay($amount) {
+        $date_interval = DateInterval::createFromDateString("$amount days");
+        $this->date_time = $this->date_time->add($date_interval);
+        $this->changeDateInfo();
+        return $this;
+    }
+
+    public function subDay($amount) {
+        $date_interval = DateInterval::createFromDateString("$amount days");
+        $this->date_time = $this->date_time->sub($date_interval);
+        $this->changeDateInfo();
+        return $this;
+    }
+
+    public function addMonth($amount) {
+        $date_interval = DateInterval::createFromDateString("$amount months");
+        $this->date_time = $this->date_time->add($date_interval);
+        $this->changeDateInfo();
+        return $this;
+    }
+
+    public function subMonth($amount) {
+        $date_interval = DateInterval::createFromDateString("$amount months");
+        $this->date_time = $this->date_time->sub($date_interval);
+        $this->changeDateInfo();
+        return $this;
+    }
+
+    public function addYear($amount) {
+        $date_interval = DateInterval::createFromDateString("$amount years");
+        $this->date_time = $this->date_time->add($date_interval);
+        $this->changeDateInfo();
+        return $this;
+    }
+
+    public function subYear($amount) {
+        $date_interval = DateInterval::createFromDateString("$amount years");
+        $this->date_time = $this->date_time->sub($date_interval);
+        $this->changeDateInfo();
+        return $this;
+    }
+
+    public function format($format) {
+        return $this->date_time->format($format);
+    }
+
+    private function changeDateInfo() {
+        $this->date_info = getdate($this->date_time->getTimestamp());
+    }
+
+}
