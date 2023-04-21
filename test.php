ecca074491120eef553873c6553ca65478abf37c
safo4eg <stas.rakov111@mail.ru>
2023-04-21 11:43:47 +0300

    Создан класс Tag, который будет служить для создания тегов средствами php. Реализовано: конструктор записывает имя тега в $name, open() - возвращает открывающую часть тега, close() - возвращает закрывающую часть тега
    В test.php продемонстрировано использование данного класса.

--- test.php
+++ test.php
@@ -1,4 +1,7 @@
 <?php
     error_reporting(E_ALL);
     ini_set('display_errors', 'on');
-    require_once('classes/File.php');
+    require_once('classes/Tag.php');
+
+    $tag = new Tag('div');
+    echo $tag->open()."text".$tag->close();
