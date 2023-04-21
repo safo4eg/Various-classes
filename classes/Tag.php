78e249e1746de4dd47c4dd72c40cf0b6897936e3
safo4eg <stas.rakov111@mail.ru>
2023-04-21 11:52:07 +0300

    Tag.php - добавлен метод getAttrsStr, который принимает атрибуты тега в виде ассоциативного массива, где ключ=имя_атрибута, а значение=значение_атрибута. Изменён конструктор, вторым параметром теперь принимает ассоциативный массив атрибутов. Изменен метод open(), теперь добавляет атрибуты (при наличие)

--- classes/Tag.php
+++ classes/Tag.php
@@ -2,18 +2,33 @@
 
 class Tag {
     private $name;
+    private $attrs;
 
-    public function __construct($name) {
+    public function __construct($name, $attrs = []) {
         $this->name = $name;
+        $this->attrs = $attrs;
     }
 
     public function open() {
         $name = $this->name;
-        return "<$name>";
+        $attrs_str = $this->getAttrsStr($this->attrs);
+        return "<$name$attrs_str>";
     }
 
     public function close() {
         $name = $this->name;
         return "</$name>";
+    }
+
+    private function getAttrsStr($attrs) {
+        if(!empty($attrs)) {
+            $result = '';
+            foreach($attrs as $name => $value) {
+                $result .= " $name=\"$value\"";
+            }
+            return $result;
+        } else {
+            return '';
+        }
     }
 }
